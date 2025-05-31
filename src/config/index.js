76984054aea4ddd3e74715bb8f8d708022a34948
Kamran <kamrankhan815.com@gmail.com>
2025-05-31 11:52:35 +0500

    add user and video model

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,17 @@ const dotenv = require("dotenv").config();
 const PORT = process.env.PORT;
 const DB_URL = process.env.DB_URL;
 const CORS_ORIGIN = process.env.CORS_ORIGIN;
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY;
 
-module.exports = { PORT, DB_URL, CORS_ORIGIN };
+module.exports = {
+  PORT,
+  DB_URL,
+  CORS_ORIGIN,
+  ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRY,
+  REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRY,
+};
